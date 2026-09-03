39323ae6f4c2fd3965ec335126ec5c591c5421dc
colombefioren <colomberakotonjanahary@gmail.com>
2026-09-03 16:20:06 +0300

    fix: add fallback

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -4,4 +4,9 @@ return {
     "clangd",
     "--query-driver=/run/current-system/sw/bin/gcc",
   },
+  init_options = {
+    fallbackFlags = {
+      "--target=x86_64-unknown-linux-gnu",
+    },
+  },
 }
