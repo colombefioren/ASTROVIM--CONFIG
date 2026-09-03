a3e27cb05f16ab5ea9ff362ddf14211af5eed0db
colombefioren <colomberakotonjanahary@gmail.com>
2026-09-03 15:19:46 +0300

    refactor: keep none ls minimal

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,7 +2,7 @@
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
-    local null_ls = require("null-ls")
+    local null_ls = require "null-ls"
 
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       null_ls.builtins.formatting.stylua,
