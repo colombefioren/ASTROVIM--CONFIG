3238115e4eb05c954bb2827c582e3b1d9faaa06a
colombefioren <colomberakotonjanahary@gmail.com>
2026-09-03 15:12:44 +0300

    chore: remove catpuccin color scheme

--- lua/community.lua
+++ lua/community.lua
@@ -1,7 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
