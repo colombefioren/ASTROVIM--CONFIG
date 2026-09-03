ae2cfa3048ff2dadf2725669dadc0e210c7c8445
colombefioren <colomberakotonjanahary@gmail.com>
2026-09-03 15:11:17 +0300

    fix: correct wrong syntax

--- lua/community.lua
+++ lua/community.lua
@@ -2,8 +2,6 @@ return {
   "AstroNvim/astrocommunity",
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-
-  { import = "astrocommunity.pack.c" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
